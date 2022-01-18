a91721f4b8f000c13bddf3f7e340c5bb0f5b9bab
StarGazer1915 <74311615+StarGazer1915@users.noreply.github.com>
2022-01-18 14:40:35 +0100

    Added classes for traffic simulation

--- src/simulation/vialis_models.py
+++ src/simulation/vialis_models.py
@@ -1,29 +1,30 @@
 # import numpy as np
-# from mesa import Agent, Model
+from mesa import Agent, Model
 # from mesa.space import MultiGrid
 # from mesa.time import RandomActivation
 # from mesa.visualization.modules.TextVisualization import TextElement
 # from numpy.lib.function_base import angle
 
-class Vehicle:
-    def __init__(self, type, speed, pos):
+
+class agent(Agent):
+    def __init__(self, type, speed, direction, pos):
         self.type = type
         self.speed = speed
+        self.direction = direction
         self.pos = pos
 
 
-class biker:
-    def __init__(self, speed, pos):
-        self.speed = speed
-        self.pos = pos
-
-
-class trafficlight:
+class traffic_light(Agent):
     def __init__(self, id):
         self.id = id
 
 
-class Enviroment:
+class sensor(Agent):
+    def __init__(self, id):
+        self.id = id
+
+
+class enviroment(Model):
     def __init__(self):
         pass
 
