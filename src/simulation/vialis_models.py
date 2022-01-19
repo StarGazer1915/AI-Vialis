593a8f645ea7956697a84a321220719c326979eb
StarGazer1915 <74311615+StarGazer1915@users.noreply.github.com>
2022-01-19 14:14:22 +0100

    Created basic visual traffic simulation (Time/Steps and functionality will follow)

--- src/simulation/vialis_models.py
+++ src/simulation/vialis_models.py
@@ -1,8 +1,9 @@
-# import numpy as np
+import numpy as np
 from turtle import width
 from mesa import Agent, Model
 from mesa.space import MultiGrid
 from mesa.time import RandomActivation
+import random
 # from mesa.visualization.modules.TextVisualization import TextElement
 # from numpy.lib.function_base import angle
 
@@ -12,44 +13,44 @@ class agent(Agent):
         super().__init__(unique_id, model)
         self.type = 'agent'
 
-    def move(self):
-        pass
-
 
 class traffic_light(Agent):
     def __init__(self, unique_id: int, model: Model):
         super().__init__(unique_id, model)
         self.type = 'tl'
 
-    def cycle(self):
-        pass
-
 
 class sensor(Agent):
-    def __init__(self, id):
-        self.id = id
-    
-    def detect(self):
-        pass
+    def __init__(self, unique_id: int, model: Model):
+        super().__init__(unique_id, model)
+        self.type = 'sensor'
 
 
 class Enviroment(Model):
-    def __init__(self, a, tl_nums, width, height) -> None:
+    def __init__(self, a, tl_nums, sen_nums, width, height) -> None:
         self.agent_nums = a
         self.tl_nums = tl_nums
+        self.sen_nums = sen_nums
         self.tick = 0
         self.running = True
         self.grid = MultiGrid(width, height, False)
         self.schedule = RandomActivation(self)
+        self.st_pos = [(0, 6), (0, 5), (0, 4), (7, 0), (8, 0), (14, 9), (14, 8), (14, 7)]
+        self.tl_pos = [(5, 6), (5, 5), (5, 4), (9, 7), (9, 8), (9, 9), (7, 3), (8, 3)]
+        self.sens_pos = [(4, 6), (4, 5), (4, 4), (10, 7), (10, 8), (10, 9), (7, 2), (8, 2)]
 
         for i in range(self.agent_nums):
             a = agent(i, self)
             self.schedule.add(a)
-            start_cell = (0, 7)
+            start_cell = random.choice(self.st_pos)
             self.grid.place_agent(a, start_cell)
 
-        for i in range(self.tl_nums):
+        for i in range(len(self.tl_pos)):
             a = traffic_light(i + self.agent_nums, self)
             self.schedule.add(a)
-            start_cell = (14, 7)
-            self.grid.place_agent(a, start_cell)
+            self.grid.place_agent(a, self.tl_pos[i])
+
+        for i in range(len(self.sens_pos)):
+            a = sensor(i + self.agent_nums + len(self.tl_pos), self)
+            self.schedule.add(a)
+            self.grid.place_agent(a, self.sens_pos[i])
--- src/simulation/vialis_server.py
+++ src/simulation/vialis_server.py
@@ -18,22 +18,24 @@ from mesa.visualization.UserParam import UserSettableParameter
 
 # ========== REGULAR FUNCTIONS ========== #
 def person_portrayal(agent):
-    portrayal = {
-        "Shape": "arrowHead",
-        "Filled": "true",
-        "scale": 0.8,
-        "heading_x": 1,
-        "heading_y": 0
-    }
-
     if agent.type == 'agent':
-        portrayal["Color"] = "black"
-        portrayal["Layer"] = 1
+        portrayal = {"Shape": "arrowHead", "scale": 0.8, "Color": "blue", "Filled": "true", "Layer": 4}
+
+        if agent.pos in [(0, 6), (0, 5), (0, 4)]:
+            portrayal["heading_x"], portrayal["heading_y"] = 1, 0
+        elif agent.pos in [(7, 0), (8, 0)]:
+            portrayal["heading_x"], portrayal["heading_y"] = 0, 1
+        elif agent.pos in [(14, 9), (14, 8), (14, 7)]:
+            portrayal["heading_x"], portrayal["heading_y"] = -1, 0
+
     elif agent.type == 'tl':
-        portrayal["Color"] = "red"
-        portrayal["Layer"] = 1
+        portrayal = {"Shape": "rect", "w": 0.75, "h": 0.75, "Color": "red", "Filled": "true", "Layer": 3}
+
+    elif agent.type == 'sensor':
+        portrayal = {"Shape": "circle", "r": 0.5, "Color": "black", "Layer": 2}
 
     return portrayal
+
 
 g_width = 15
 g_height = 11
@@ -42,12 +44,14 @@ g_height = 11
 grid = CanvasGrid(person_portrayal, g_width, g_height, 600, 400)
 number_of_agents_slider = UserSettableParameter('slider', 'Agents', 10, 1, 20, 1)
 number_of_traffic_lights_slider = UserSettableParameter('slider', 'Traffic Lights', 4, 1, 8, 1)
+number_of_sensors_slider = UserSettableParameter('slider', 'Sensors', 4, 1, 8, 1)
 
 # ============ SERVER SETUP ============= #
 server = ModularServer(Enviroment, [grid], "Traffic Simulation",
                         {
-                            "a": number_of_agents_slider,
-                            "tl_nums": number_of_traffic_lights_slider,
+                            "a":        number_of_agents_slider,
+                            "tl_nums":  number_of_traffic_lights_slider,
+                            "sen_nums": number_of_sensors_slider,
                             "width":    g_width,
                             "height":   g_height
                         })
