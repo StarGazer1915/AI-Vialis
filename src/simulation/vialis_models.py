cf8dae161b70f8f320540c3063e320747bd12f2f
UnknownZandbak <rjwillar01@gmail.com>
2022-01-20 10:51:41 +0100

    Esxtra kruispunt en random movement

--- src/simulation/vialis_models.py
+++ src/simulation/vialis_models.py
@@ -1,56 +1,117 @@
-import numpy as np
+# import numpy as np
 from turtle import width
 from mesa import Agent, Model
 from mesa.space import MultiGrid
 from mesa.time import RandomActivation
-import random
 # from mesa.visualization.modules.TextVisualization import TextElement
 # from numpy.lib.function_base import angle
 
-
 class agent(Agent):
-    def __init__(self, unique_id: int, model: Model):
-        super().__init__(unique_id, model)
-        self.type = 'agent'
+    def __init__(self, pos, model):
+        super().__init__(pos, model)
+        #self.type = type
+        #self.speed = speed
+        #self.direction = direction
+        self.pos = pos
+        self.shape = (0.75, 0.75)
+        self.layer = 2
+        self.color = "blue"
+        self.model = model
 
+    def step(self):
+        self.move()
+
+    def move(self):
+        """"Move the agent to a rand spot near him"""
+        possible_poss = self.model.grid.get_neighborhood(self.pos, moore=True, include_center=False)
+        print(possible_poss)
+        new_pos = self.random.choice(possible_poss)
+        self.model.grid.move_agent(self, new_pos)
+
+class road(Agent):
+    def __init__(self, pos, shape):
+        self.pos = pos
+        self.layer = 0
+        self.shape = shape
+        self.color = "black"
 
 class traffic_light(Agent):
-    def __init__(self, unique_id: int, model: Model):
-        super().__init__(unique_id, model)
-        self.type = 'tl'
+    def __init__(self, unique_id, pos, shape):
+        self.unique_id = unique_id
+        self.pos = pos
+        self.layer = 1
+        self.shape = shape
+        self.color = "red"
 
+    def cycle(self):
+        pass
 
 class sensor(Agent):
-    def __init__(self, unique_id: int, model: Model):
-        super().__init__(unique_id, model)
-        self.type = 'sensor'
+    def __init__(self, unique_id, pos, shape):
+        self.unique_id = unique_id
+        self.pos = pos
+        self.layer = 1
+        self.shape = shape
+        self.color = "gray"
+    
+    def detect(self):
+        pass
 
-
-class Enviroment(Model):
-    def __init__(self, a, tl_nums, sen_nums, width, height) -> None:
-        self.agent_nums = a
-        self.tl_nums = tl_nums
-        self.sen_nums = sen_nums
-        self.tick = 0
-        self.running = True
+class enviroment(Model):
+    def __init__(self, width, height, afs_sto_1_3_sen, afs_sto_4_6_sen, afs_sto_7_sen, afs_sto_8_11_sen,
+                 afs_sto_12_14_sen, afs_sto_15_16_sen):
         self.grid = MultiGrid(width, height, False)
         self.schedule = RandomActivation(self)
-        self.st_pos = [(0, 6), (0, 5), (0, 4), (7, 0), (8, 0), (14, 9), (14, 8), (14, 7)]
-        self.tl_pos = [(5, 6), (5, 5), (5, 4), (9, 7), (9, 8), (9, 9), (7, 3), (8, 3)]
-        self.sens_pos = [(4, 6), (4, 5), (4, 4), (10, 7), (10, 8), (10, 9), (7, 2), (8, 2)]
+        self.running = True
 
-        for i in range(self.agent_nums):
-            a = agent(i, self)
-            self.schedule.add(a)
-            start_cell = random.choice(self.st_pos)
-            self.grid.place_agent(a, start_cell)
+        traffic_light_count = 0
+        sensor_count = 0
+        for (contents, x, y) in self.grid.coord_iter():
+            # Horizontal road
+            if (0 <= x <= 24 and y in [39, 38]) or (0 <= x <= 25 and y == 37) or (
+                    30 <= x <= 55 and y in [16, 17, 18, 20]) or (29 <= x <= 55 and y == 19):
+                self.grid._place_agent((x, y), road((x, y), (1, 0.5)))
+            # Vertical road
+            elif (x in [26, 29] and (0 <= y <= 15 or 21 <= y <= 36 or 40 <= y <= 55)) or (
+                        x == 25 and 40 <= y <= 55) or (x == 27 and (21 <= y <= 36 or 40 <= y <= 55)) or (
+                             x == 28 and (0 <= y <= 15 or 21 <= y <= 36)) or (
+                             x == 30 and (29 <= y <= 36 or 40 <= y <= 55)):
+                self.grid._place_agent((x, y), road((x, y), (0.5, 1)))
 
-        for i in range(len(self.tl_pos)):
-            a = traffic_light(i + self.agent_nums, self)
-            self.schedule.add(a)
-            self.grid.place_agent(a, self.tl_pos[i])
+            # Traffic lights in horizontal road
+            if (x == 24 and 37 <= y <= 39) or (x == 30 and 18 <= y <= 20):
+                new_traffic_light = traffic_light(traffic_light_count, (x, y), (0.5, 0.75))
+                self.grid._place_agent((x, y), new_traffic_light)
+                self.schedule.add(new_traffic_light)
+                traffic_light_count += 1
+            # Traffic lights in vertical road
+            elif (x in [28, 29] and y in [15, 36]) or ((x, y) == (30, 36)) or (x in [26, 27] and y in [21, 40]) or (
+                        (x, y) == (25, 40)):
+                new_traffic_light = traffic_light(traffic_light_count, (x, y), (0.75, 0.5))
+                self.grid._place_agent((x, y), new_traffic_light)
+                self.schedule.add(new_traffic_light)
+                traffic_light_count += 1
 
-        for i in range(len(self.sens_pos)):
-            a = sensor(i + self.agent_nums + len(self.tl_pos), self)
-            self.schedule.add(a)
-            self.grid.place_agent(a, self.sens_pos[i])
+            # Sensors in horizontal road
+            if (x in [23, 23 - afs_sto_4_6_sen] and 37 <= y <= 39) or (
+                    x in [31, 31 + afs_sto_12_14_sen] and 18 <= y <= 20):
+                new_sensor = sensor(sensor_count, (x, y), (0.1, 0.3))
+                self.grid._place_agent((x, y), new_sensor)
+                sensor_count += 1
+            # Sensors in vertical road
+            elif (25 <= x <= 27 and y in [41, 41 + afs_sto_1_3_sen]) or (
+                        x in [26, 27] and y in [22, 22 + afs_sto_8_11_sen]) or (
+                             x in [28, 29] and y in [35, 35 - afs_sto_8_11_sen]) or (
+                             x == 30 and y in [35, 35 - afs_sto_7_sen]) or (
+                             x in [28, 29] and y in [14, 14 - afs_sto_15_16_sen]):
+                new_sensor = sensor(sensor_count, (x, y), (0.3, 0.1))
+                self.grid._place_agent((x, y), new_sensor)
+                sensor_count += 1
+
+            if (x, y) == (1, 39):
+                new_agent = agent((x, y), self)
+                self.grid._place_agent((x, y), new_agent)
+                self.schedule.add(new_agent)
+
+    def step(self):
+        self.schedule.step()
--- src/simulation/vialis_server.py
+++ src/simulation/vialis_server.py
@@ -10,49 +10,42 @@ The creation of the sliders can be found under the variables section!
 
 # =============== IMPORTS =============== #
 from turtle import width
-from vialis_models import Enviroment
+from vialis_models import enviroment
 from mesa.visualization.modules import CanvasGrid
 from mesa.visualization.ModularVisualization import ModularServer
 from mesa.visualization.UserParam import UserSettableParameter
 
-
 # ========== REGULAR FUNCTIONS ========== #
 def person_portrayal(agent):
-    if agent.type == 'agent':
-        portrayal = {"Shape": "arrowHead", "scale": 0.8, "Color": "blue", "Filled": "true", "Layer": 4}
+    (x, y) = agent.pos
+    (w, h) = agent.shape
+    return {"Shape": "rect",
+            "w": w,
+            "h": h,
+            "Layer": agent.layer,
+            "Filled": "true",
+            "x": x,
+            "y": y,
+            "Color": agent.color}
 
-        if agent.pos in [(0, 6), (0, 5), (0, 4)]:
-            portrayal["heading_x"], portrayal["heading_y"] = 1, 0
-        elif agent.pos in [(7, 0), (8, 0)]:
-            portrayal["heading_x"], portrayal["heading_y"] = 0, 1
-        elif agent.pos in [(14, 9), (14, 8), (14, 7)]:
-            portrayal["heading_x"], portrayal["heading_y"] = -1, 0
-
-    elif agent.type == 'tl':
-        portrayal = {"Shape": "rect", "w": 0.75, "h": 0.75, "Color": "red", "Filled": "true", "Layer": 3}
-
-    elif agent.type == 'sensor':
-        portrayal = {"Shape": "circle", "r": 0.5, "Color": "black", "Layer": 2}
-
-    return portrayal
-
-
-g_width = 15
-g_height = 11
-
+g_width = 56
+g_height = 56
 # ============== VARIABLES ============== #
-grid = CanvasGrid(person_portrayal, g_width, g_height, 600, 400)
-number_of_agents_slider = UserSettableParameter('slider', 'Agents', 10, 1, 20, 1)
-number_of_traffic_lights_slider = UserSettableParameter('slider', 'Traffic Lights', 4, 1, 8, 1)
-number_of_sensors_slider = UserSettableParameter('slider', 'Sensors', 4, 1, 8, 1)
+grid = CanvasGrid(person_portrayal, g_width, g_height, g_width * 15, g_height * 15)
+model_params = {"height": g_width,
+                "width": g_height,
+                "afs_sto_1_3_sen": UserSettableParameter("slider", "Afstand tussen stoplichten 1 t/m 3 en sensoren",
+                                                                      value=7, min_value=1, max_value=14, step=1),
+                "afs_sto_4_6_sen": UserSettableParameter("slider", "Afstand tussen stoplichten 4 t/m 6 en sensoren",
+                                                                      value=12, min_value=1, max_value=23, step=1),
+                "afs_sto_7_sen": UserSettableParameter("slider", "Afstand tussen stoplicht 7 en sensor",
+                                                                      value=3, min_value=1, max_value=6, step=1),
+                "afs_sto_8_11_sen": UserSettableParameter("slider", "Afstand tussen stoplichten 8 t/m 11 en sensoren",
+                                                                      value=7, min_value=1, max_value=14, step=1),
+                "afs_sto_12_14_sen": UserSettableParameter("slider", "Afstand tussen stoplichten 12 t/m 14 en sensoren",
+                                                                      value=12, min_value=1, max_value=24, step=1),
+                "afs_sto_15_16_sen": UserSettableParameter("slider", "Afstand tussen stoplichten 15 & 16 en sensoren",
+                                                                      value=7, min_value=1, max_value=14, step=1)}
 
 # ============ SERVER SETUP ============= #
-server = ModularServer(Enviroment, [grid], "Traffic Simulation",
-                        {
-                            "a":        number_of_agents_slider,
-                            "tl_nums":  number_of_traffic_lights_slider,
-                            "sen_nums": number_of_sensors_slider,
-                            "width":    g_width,
-                            "height":   g_height
-                        })
-
+server = ModularServer(enviroment, [grid], "Vialis", model_params)
